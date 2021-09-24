cb638d0e3eedde4c9414a6699ad030edae78e59e
Santiago De Olivera <santiagodeolivera@gmail.com>
2021-09-24 19:41:36 -0300

    Add comments

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Full_GRASP_And_SOLID.Library
 {
+    /// <summary>
+    /// This class represents a printer which prints into the console.
+    /// </summary>
     public class ConsolePrinter: IPrinter
     {
         void IPrinter.PrintRecipe(string recipeText)
--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -3,6 +3,9 @@ using System.IO;
 
 namespace Full_GRASP_And_SOLID.Library
 {
+    /// <summary>
+    /// This class represents a printer which prints into a file in the local file system.
+    /// </summary>
     public class FilePrinter: IPrinter
     {
         private string _path;
